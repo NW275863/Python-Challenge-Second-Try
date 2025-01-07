0411901f118231a9115445003b31d4d431a5293f
NW275863 <nwarrenmse@gmail.com>
2025-01-07 00:24:33 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,6 @@
-
 import csv
 
-
-
+# budget data from the CSV file
 with open('budget_data.csv', 'r') as file:
     reader = csv.reader(file)
     next(reader)  # Skip the header row
@@ -15,27 +13,27 @@ with open('budget_data.csv', 'r') as file:
         months.append(row[0])
         profit_losses.append(int(row[1]))
 
+# number of months
 total_months = len(months)
 
-
+# net total amount of profit/losses
 net_total = sum(profit_losses)
 
-
+#  changes in profit/losses and store them in a list
 for i in range(1, total_months):
     change = profit_losses[i] - profit_losses[i-1]
     changes.append(change)
 
-
+# average change
 average_change = sum(changes) / len(changes)
 
-
-
+# greatest increase and decrease in profits
 greatest_increase = max(changes)
 greatest_increase_date = months[changes.index(greatest_increase) + 1]
 greatest_decrease = min(changes)
 greatest_decrease_date = months[changes.index(greatest_decrease) + 1]
 
-
+# Print the analysis results
 print("Financial Analysis")
 print("-----------------------------")
 print(f"Total Months: {total_months}")
@@ -43,9 +41,3 @@ print(f"Total: ${net_total}")
 print(f"Average Change: ${average_change:.2f}")
 print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
 print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
-
-
-
-
-
-
